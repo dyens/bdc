6daacfaec6abeffe45f64aef2637801d2931ba6e
dyens <dyens@mail.ru>
2019-11-28 16:13:52 +0000

    :sparkles: Add mypy and flake settings

--- bdc/node.py
+++ bdc/node.py
@@ -1,24 +1,60 @@
 """Node implementation."""
 
+from typing import (
+    List,
+    Optional,
+)
+
+
 class Node:
     """Node.
-    
+
     Every node have parent except root node.
     """
 
     def __init__(self, value: str):
         """Init a new node."""
         self.value = value
+        self.node_id = None
 
         self._is_deleted = False
-        self._node_id = None
-        self._childs = []
-        self._parent = None
+        self._childs: List['Node'] = []
+        self._parent: Optional['Node'] = None
 
     @property
     def is_deleted(self):
         """Is deleted."""
         return self._is_deleted
+
+    @property
+    def parent(self):
+        """Parent."""
+        return self._parent
+
+    @property
+    def childs(self):
+        """Childs."""
+        return self._childs
+
+    def append_child(self, child: 'Node'):
+        """Add node to child list."""
+        child._set_parent(self)  # NOQA:WPS437
+        self._childs.append(child)
+
+    @property
+    def all_childs(self):
+        """Get all childs recursively."""
+        childs = [child for child in self._childs]
+        while childs:
+            child = childs.pop()
+            childs.extend(child._childs)  # NOQA:WPS437
+            yield child
+
+    def delete(self):
+        """Delete node."""
+        self._is_deleted = True
+        for child in self.all_childs:
+            child._is_deleted = True  # NOQA:WPS437
 
     def _set_parent(self, parent: 'Node'):
         """Set parent.
@@ -28,24 +64,3 @@ class Node:
         if self._parent is not None:
             raise ValueError('This node already have parent')
         self._parent = parent
-
-    def append_child(self, child: 'Node'):
-        """Add node to child list."""
-        child._set_parent(self)
-        self._childs.append(child)
-
-
-    @property
-    def all_childs(self):
-        """Get all childs recursively."""
-        childs = [i for i in self._childs]
-        while childs:
-            child = childs.pop()
-            childs.extend(child._childs)
-            yield child
-
-    def delete(self):
-        """Delete node."""
-        self._is_deleted = True
-        for child in self.all_childs:
-            child._is_deleted = True
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,5 +1,6 @@
-from bdc.node import Node
 import pytest
+
+from bdc.node import Node
 
 
 class TestNode:
@@ -10,6 +11,7 @@ class TestNode:
     def new_node():
         """Simple node fixture."""
         node_id = 0
+
         def _create_node():
             """Create new node."""
             nonlocal node_id
@@ -21,11 +23,10 @@ class TestNode:
         """Testing node initialization."""
         node = new_node()
         assert node.value == 'val1'
-        assert node._is_deleted is False
-        assert node._node_id is None
-        assert node._childs == []
-        assert node._parent is None
-
+        assert node.is_deleted is False
+        assert node.node_id is None
+        assert not node.childs
+        assert node.parent is None
 
     def test__set_parent(self, new_node):
         """Testing set parent."""
@@ -50,8 +51,8 @@ class TestNode:
         parent = new_node()
         child = new_node()
         parent.append_child(child)
-        assert parent._childs == [child]
-        assert child._parent == parent
+        assert parent.childs == [child]
+        assert child.parent == parent
 
     def test_append_child_fail_double_append(self, new_node):
         """Testing append child.
@@ -72,8 +73,7 @@ class TestNode:
         parent.append_child(child)
         grand_parent.append_child(parent)
 
-        assert list(grand_parent.all_childs)== [parent, child]
-        
+        assert list(grand_parent.all_childs) == [parent, child]
 
     def test_delete(self, new_node):
         """Test delete."""
@@ -84,7 +84,6 @@ class TestNode:
         grand_parent.append_child(parent)
 
         grand_parent.delete()
-        assert grand_parent.is_deleted == True
-        assert parent.is_deleted == True
-        assert child.is_deleted == True
-        
+        assert grand_parent.is_deleted is True
+        assert parent.is_deleted is True
+        assert child.is_deleted is True
