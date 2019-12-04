50d86efc76d99ee64ad16d8f2d56e0838895ef58
dyens <dyens@mail.ru>
2019-12-04 16:14:39 +0300

    :sparkles: Implement Cache struct

--- bdc/cache.py
+++ bdc/cache.py
@@ -1,32 +1,97 @@
-"""Cache of tree implementation."""
+"""Cache of db implementation."""
 
-from bdc.tree import Tree
+from bdc.db import DB
+from bdc.node import CNode
 
 
 class Cache:
-    """Tree cache."""
+    """DB cache."""
 
     def __init__(self):
         """Initialization."""
-        self.nodes = {}
-        self.new_nodes = []  # Nodes without node_id
+        # Nodes  db index
+        self.db_nodes = {}
+        # Nodes  cache  index
+        self.cache_nodes = {}
+        self._cache_index = 0
+        super().__init__()
 
-    def load(self, node_id: int, tree: Tree):
-        """Load node from tree."""
-        if node_id in self.nodes:
+    def delete(self, cache_id: int):
+        """Delete nodes from cache."""
+        node = self.cache_nodes[cache_id]
+        node.delete()
+
+    default_name = 'New Node'
+
+    def add_node(self, parent_cache_id: int) -> CNode:
+        """Add new node to parent."""
+        parent = self.cache_nodes[parent_cache_id]
+        cache_id = self._cache_index
+        self._cache_index += 1
+
+        new_node = CNode(
+            cache_id=cache_id,
+            value=self.default_name,
+            db_id=None,
+            is_deleted=False,
+        )
+        parent.append_child(new_node)
+        self.cache_nodes[cache_id] = new_node
+        return new_node
+
+    def load(self, db_id: int, db: DB):
+        """Load node from db."""
+        # node already loaded
+        if db_id in self.db_nodes:
             return
 
-        # crete node copy
-        new_node = tree.get_node_copy(node_id)
-        self.nodes[new_node.data.node_id] = new_node
+        # create node copy
+        new_node_params = db.get_node_params(db_id)
+        db_id = new_node_params['db_id']
+        cache_id = self._cache_index
+        self._cache_index += 1
+        new_node = CNode(cache_id, **new_node_params)
+        self.db_nodes[db_id] = new_node
+        self.cache_nodes[cache_id] = new_node
 
-        # restore node connections and is_deleted attribute
-        parent_id = tree.get_parent_id(node_id)
-        if parent_id in self.nodes:
-            parent = self.nodes[parent_id]
-            new_node.data.is_deleted = parent.data.is_deleted
+        # restore node connections
+
+        # restore parent and is_deleted attribute
+        # if parent deleted node should be deleted to
+        parent_id = db.get_parent_id(db_id)
+        if parent_id in self.db_nodes:
+            parent = self.db_nodes[parent_id]
+            new_node.is_deleted = parent.is_deleted
             parent.append_child(new_node)
 
-    def save(self, tree: Tree):
-        """Save cache to tree."""
-        raise NotImplementedError
+        # restore child connections
+        child_ids = db.get_children_ids(db_id)
+        for child_id in child_ids:
+            if child_id in self.db_nodes:
+                child = self.db_nodes[child_id]
+                new_node.append_child(child)
+
+    def save(self, db: DB):
+        """Save cache to db."""
+        # new created nodes appended to the end of dict
+        # in python >= 3.6 dicts are ordered
+        for _cache_id, node in self.cache_nodes.items():
+            db_id = node.db_id
+            if db_id:
+                db.update_node(
+                    db_id,
+                    node.value,
+                    node.is_deleted,
+                )
+                continue
+
+            parent = node.parent
+            if parent is None:
+                raise RuntimeError('In cache all new nodes is subnodes')
+
+            new_node = db.add_to_parent(
+                parent.db_id,
+                node.value,
+                node.is_deleted,
+            )
+            self.db_nodes[new_node.db_id] = node
---
+++ tests/test_cache.py
@@ -0,0 +1,140 @@
+import pytest
+
+from bdc.cache import Cache
+from bdc.db import DB
+
+
+class TestCache:
+    """Cache testing."""
+
+    @staticmethod
+    @pytest.fixture
+    def cache():
+        """Simple cache fixture.
+
+        0 root
+        1   node_1_1
+        3     node_2_1
+        5       node_3_1
+        7         node_4_1
+        8         node_4_2
+        6       node_3_2
+        4     node_2_2
+        2   node_1_2
+        """
+        db = DB.default()
+        new_cache = Cache()
+        for db_id in db.nodes:
+            new_cache.load(db_id, db)
+        return new_cache
+
+    def test_delete(self, cache):
+        """Test delete."""
+        # delete node_2_1
+        cache.delete(3)
+        # Parent not deleted
+        assert cache.cache_nodes[1].is_deleted is False
+        # Node with childs deleted
+        assert cache.cache_nodes[3].is_deleted is True
+        assert cache.cache_nodes[5].is_deleted is True
+        assert cache.cache_nodes[7].is_deleted is True
+
+    def test_add_node(self, cache):
+        """Test add_node."""
+        new_node = cache.add_node(5)
+        assert cache.cache_nodes[9] == new_node
+        assert new_node.cache_id == 9
+        assert new_node.value == Cache.default_name
+        assert new_node.is_deleted is False
+
+    def test_load(self):
+        """Test load from db."""
+        db = DB.default()
+        cache = Cache()
+        cache.load(5, db)
+
+        m_node = cache.cache_nodes[0]
+        assert m_node.value == 'node_3_1'
+        assert m_node.parent is None
+        assert not m_node.childs
+
+        cache.load(7, db)
+
+        l_node = cache.cache_nodes[1]
+        assert l_node.value == 'node_4_1'
+        assert l_node.parent is m_node
+        assert not l_node.childs
+        assert m_node.childs == [l_node]
+
+        cache.load(3, db)
+
+        h_node = cache.cache_nodes[2]
+        assert h_node.value == 'node_2_1'
+        assert h_node.parent is None
+        assert h_node.childs == [m_node]
+        assert m_node.parent == h_node
+
+    def test_save(self):
+        """Test save to db."""
+        db = DB.default()
+        cache = Cache()
+        cache.load(3, db)
+        cache.load(5, db)
+        cache.load(7, db)
+        cache.add_node(1)
+        cache.delete(1)
+        cache.cache_nodes[1].value = 'new_value'
+
+        # Cache struct:
+        # id del cid value
+        # 3  f   0   node_2_1
+        # 5  t   1     node_value
+        # 7  t   2       node_4_1
+        # ?  t   3       New Node
+
+        cache.save(db)
+        assert len(cache.db_nodes) == len(cache.cache_nodes)
+
+        new_node = cache.db_nodes[9]
+        assert cache.cache_nodes[3] == new_node
+        assert new_node.value == 'New Node'
+
+        # DB struct:
+        # id del cid value
+        # 0  f   ?   root
+        # 1  f   ?     node_1_1
+        # 3  f   0       node_2_1
+        # 5  t   1         node_value
+        # 7  t   2           node_4_1
+        # 8  t   3           node_4_2
+        # 9  t   4           New Node
+        # 6  f   ?         node_3_2
+        # 4  f   ?       node_2_2
+        # 2  f   ?     node_1_2
+        node_1_1 = db.nodes[1]
+        node_2_1 = db.nodes[3]
+        node_3_2 = db.nodes[6]
+        node_modified = db.nodes[5]
+        node_4_1 = db.nodes[7]
+        node_4_2 = db.nodes[8]
+        new_node = db.nodes[9]
+
+        assert new_node.is_deleted is True
+        assert new_node.value == 'New Node'
+        assert not new_node.childs
+        assert new_node.parent == node_modified
+
+        assert node_4_1.is_deleted is True
+        assert node_4_1.value == 'node_4_1'
+        assert not node_4_1.childs
+        assert node_4_1.parent == node_modified
+
+        assert node_modified.is_deleted is True
+        assert node_modified.value == 'new_value'
+        assert set(node_modified.childs) == {node_4_1, node_4_2, new_node}
+        assert node_modified.parent == node_2_1
+
+        assert node_2_1.is_deleted is False
+        assert node_2_1.value == 'node_2_1'
+        assert set(node_2_1.childs) == {node_modified, node_3_2}
+        assert node_2_1.parent == node_1_1
